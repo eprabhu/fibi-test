0af47dccc26c536a751012a94868cadbae1a327a
Prabhu <121430466+eprabhu@users.noreply.github.com>
2026-02-12 12:51:43 +0530

    commit

---
+++ NCBS/FN_GET_CHILD_NODES.sql
@@ -0,0 +1,32 @@
+﻿DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `FN_GET_CHILD_NODES`(VALUE INT) RETURNS int
+    DETERMINISTIC
+BEGIN
+        DECLARE _ID INT;
+        DECLARE _PARENT INT;
+        DECLARE _NEXT INT;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET @ID = NULL;
+        SET _PARENT = @ID;
+        SET _ID = -1;
+        IF @ID IS NULL THEN
+                RETURN NULL;
+        END IF;
+        LOOP
+                SELECT  MIN(ID)
+                INTO    @ID
+                FROM    UNIT_TMP
+                WHERE   PARENT_ID = _PARENT
+                         AND ID > _ID;
+                IF @ID IS NOT NULL OR _PARENT = @START_WITH THEN
+                        SET @LEVEL = @LEVEL + 1;
+                        RETURN @ID;
+                END IF;
+                SET @LEVEL := @LEVEL - 1;
+                SELECT  ID, PARENT_ID
+                INTO    _ID, _PARENT
+                FROM    UNIT_TMP
+                WHERE   ID = _PARENT;
+        END LOOP;
+END
+$$
+DELIMITER ;
